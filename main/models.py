c9fe6cfc62d2ac54f6e7bfe2bb54778c96b655b4
Mederbek <kanatbekov.meder.00@gmail.com>
2021-01-17 00:42:57 +0600

    Created data base and I did hw-31. Created firstpage, secondPage, thirdPage

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ToDo (models.Model):
+    text = models.CharField(max_length = 100) # текст задачки нап: купить молоко
+    created_at = models.DateField(auto_now_add=True)  # дата
+    is_closed = models.BooleanField(default=False) # птичка если задача заверщена
+    is_favorite = models.BooleanField(default=False) # задачка важна ли
+
+
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second
+from ..main.views import homepage, test, second
 from django.conf import settings
 from django.conf.urls.static import static
 
