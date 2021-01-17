83550e363d3ab7fe3271d5dd3deed7e40224d2a1
Mederbek <kanatbekov.meder.00@gmail.com>
2021-01-17 22:01:35 +0600

    added todo list in test.html page

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDo
+
 
 def homepage(request):
     return render(request, "index.html")
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list}) # в test.html отпавляется "todo_list"
 
 def second(request):
     return HttpResponse("test2 page")
