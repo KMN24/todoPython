7185715c34233b780a3e568b592a4b3b670aba0e
Mederbek <kanatbekov.meder.00@gmail.com>
2021-01-18 15:00:30 +0600

    added todo form

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ToDo (models.Model):
     text = models.CharField(max_length = 100) # текст задачки нап: купить молоко
-    created_at = models.DateField(auto_now_add=True)  # дата
+    created_at = models.DateTimeField(auto_now_add=True)  # дата
     is_closed = models.BooleanField(default=False) # птичка если задача заверщена
     is_favorite = models.BooleanField(default=False) # задачка важна ли
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ToDo, BookShop
 
@@ -16,3 +16,11 @@ def second(request):
 def bookShop(request):
     book_shop = BookShop.objects.all()
     return render(request, "books.html", { "book_shop": book_shop })
+
+def add_todo(request):
+    form = request.POST 
+    text = form["todo_text"]
+    #print(text)
+    todo= ToDo(text=text) # 1 text - это тот который в models.py
+    todo.save()
+    return redirect(test) # обратно вернемся к странице test.html
