9b05345bf0f77d4b61b2c8dc4799dee88ba95513
Mederbek <kanatbekov.meder.00@gmail.com>
2021-01-18 23:36:48 +0600

    added todo deletion

--- main/views.py
+++ main/views.py
@@ -6,16 +6,21 @@ from .models import ToDo, BookShop
 def homepage(request):
     return render(request, "index.html")
 
+
 def test(request):
     todo_list = ToDo.objects.all()
-    return render(request, "test.html", {"todo_list": todo_list}) # в test.html отпавляется "todo_list"
+    # в test.html отпавляется "todo_list"
+    return render(request, "test.html", {"todo_list": todo_list})
+
 
 def second(request):
     return HttpResponse("test2 page")
 
+
 def bookShop(request):
     book_shop = BookShop.objects.all()
-    return render(request, "books.html", { "book_shop": book_shop })
+    return render(request, "books.html", {"book_shop": book_shop})
+
 
 def add_bookshop(request):
     form = request.POST
@@ -27,22 +32,29 @@ def add_bookshop(request):
     author = form["bookshop_author"]
     year = form["bookshop_year"]
     bookshop = BookShop(
-        title = title, 
-        subtitle = subtitle,
-        description = description,
-        price = price,
-        genre = genre,
-        author = author,
-        year = year
-        )
+        title=title,
+        subtitle=subtitle,
+        description=description,
+        price=price,
+        genre=genre,
+        author=author,
+        year=year
+    )
     bookshop.save()
     return redirect(bookShop)
 
+
 def add_todo(request):
-    form = request.POST 
+    form = request.POST
     text = form["todo_text"]
-    #print(text)
-    todo= ToDo(text=text) # 1 text - это тот который в models.py
+    # print(text)
+    todo = ToDo(text=text)  # 1 text - это тот который в models.py
     todo.save()
-    return redirect(test) # обратно вернемся к странице test.html
+    return redirect(test)  # обратно вернемся к странице test.html
     # здесь test это функция
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
